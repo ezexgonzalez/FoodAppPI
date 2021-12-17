f8423c9884b9a5d58a755a399f0e4598c55bda60
Eze <Jogo@Eze.Smash>
2021-12-16 21:22:00 -0300

    update

--- client/src/Components/NewRecipe/newRecipe.jsx
+++ client/src/Components/NewRecipe/newRecipe.jsx
@@ -1,11 +1,14 @@
-import { useState } from "react";
-import {Link} from "react-router-dom"
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom"
 import s from "./newRecipe.module.css";
 
 
 
 export default function NewRecipe() {
 
+
+
+   
 
     const [formState, setFormState] = useState({
         title: "",
@@ -14,6 +17,40 @@ export default function NewRecipe() {
         healtyLevel: 0,
         steps: []
     })
+    const [stepState, setStepState] = useState({
+        steps: "",
+        id: [1,2,3,4,5,6,7,8,9,10]
+    })
+
+
+
+     function close(number) {
+
+        stepState.id.push(number);
+
+         setFormState(prevState => {
+            return {
+                ...prevState,
+                steps: formState.steps.filter(step => step.number !== number)
+            }
+        });
+
+
+        setStepState(prevState =>{
+        
+            return{
+                ...prevState,
+                id: stepState.id.sort(function(a, b){return a - b})
+            }
+        })
+    
+    }
+
+    function orderSteps(){
+
+        return formState.steps.sort(function(a, b){return a.number - b.number})
+       
+    }
 
     function handleOnChange(e) {
 
@@ -26,27 +63,71 @@ export default function NewRecipe() {
                 }
             });
         }
+        setStepState(prevState => {
+            return {
+                ...prevState,
+                [e.target.name]: e.target.value
+            }
+        })
+
+    }
+
+    async function addStep(e) {
+
+        if(stepState.id.length > 0){
+            await setFormState(prevState => {
+                return {
+                    ...prevState,
+                    steps: [...formState.steps, {
+                        number: stepState.id[0],
+                        step: stepState.steps
+                    }]
+                }
+            });
+
+            stepState.id.shift()
+        }
+        
+    }
+
+    function submmit(e) {
+        e.preventDefault();
     }
 
     return (
-        <div>
-            
-            <div className={s.container}>
+        <form className={s.full} onSubmit={submmit}>
             <span className={s.title}>Create recipe.</span>
-                <form className={s.form} action="">
-                    
-                    <input className={s.input} placeholder="Name" onChange={handleOnChange} name="title" type="text" />
-                    <textarea placeholder="Summary" className={s.textarea} onChange={handleOnChange} name="summary" id="" cols="30" rows="10"></textarea>
-                    <input className={s.input} placeholder="Punctuation" onChange={handleOnChange} name="punctuation" type="number" max="100" min="0" />
-                    <input className={s.input} placeholder="Healty level" onChange={handleOnChange} name="healtyLevel" type="number" max="100" min="0" />
-                    <button className={s.button}>Create</button>
-                </form>
-                <Link to="/home">
-                    <span className={s.buttonBack}>Back to Home</span>
-                </Link>
-               
+            <div className={s.main}>
+                <div className={s.container}>
+                    <div className={s.form}>
+                        <input className={s.input} placeholder="Name" onChange={handleOnChange} name="title" type="text" />
+                        <textarea placeholder="Summary" className={s.textarea} onChange={handleOnChange} name="summary" id="" cols="30" rows="10"></textarea>
+                        <input className={s.input} placeholder="Punctuation" onChange={handleOnChange} name="punctuation" type="number" max="100" min="0" />
+                        <input className={s.input} placeholder="Healty level" onChange={handleOnChange} name="healtyLevel" type="number" max="100" min="0" />
+                    </div>
+
+                </div>
+                <div className={s.stepsContainer}>
+                    <div className={s.steps}>
+                        {
+                            formState.steps.length > 0 ? orderSteps().map(step => (
+                                <div className={s.stepNumber} key={step.number}>
+                                    Step {step.number}
+                                    <button onClick={() => close(step.number)} className={s.close}>X</button>
+                                </div>
+                            )) : ""
+                        }
+                    </div>
+                    <textarea onChange={handleOnChange} className={s.textarea} placeholder="Steps" name="steps" id="" cols="30" rows="10"></textarea>
+                    <button onClick={addStep} className={s.addButton}>Add</button>
+                </div>
             </div>
-        </div>
+            <button type="submit" className={s.button}>Create</button>
+            <Link to="/home">
+                <span className={s.buttonBack}>Back to Home</span>
+            </Link>
+
+        </form>
 
     )
 
